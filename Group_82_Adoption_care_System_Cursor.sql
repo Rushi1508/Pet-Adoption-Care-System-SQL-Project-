09046a06c3a12983b35ce93a9334e62dcc724cb0
Rushi Parikh <126984217+Rushi1508@users.noreply.github.com>
2025-05-02 12:10:00 -0700

    Created Cursor

---
+++ Group_82_Adoption_care_System_Cursor.sql
@@ -0,0 +1,38 @@
+/*
+    Project Title: Pet Adoption & Care System
+    Group Number: 82
+    Group Members: Rushi Vasantkumar Parikh, Mitali Kamal Bagadia
+    Course: IFT 530 - Arizona State University
+    Description: SQL Script to create cursors for Pet Adoption & Care System.
+    Date: 05/02/2025
+*/
+
+USE Group82_Pet_Adoption_Care_System;
+GO
+
+-- Declare variables
+DECLARE @petName VARCHAR(50), @status VARCHAR(20);
+
+-- Declare cursor
+DECLARE pet_cursor CURSOR FOR
+SELECT name, status
+FROM Pets;
+
+-- Open cursor
+OPEN pet_cursor;
+
+-- Fetch the first row
+FETCH NEXT FROM pet_cursor INTO @petName, @status;
+
+-- Loop through all rows
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT 'Pet: ' + @petName + ' | Status: ' + @status;
+    
+    -- Fetch next row
+    FETCH NEXT FROM pet_cursor INTO @petName, @status;
+END;
+
+-- Close and deallocate cursor
+CLOSE pet_cursor;
+DEALLOCATE pet_cursor;
